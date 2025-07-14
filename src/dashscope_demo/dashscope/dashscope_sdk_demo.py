0819be2057f605ef537a349a5ec9a378b69ce751
Ljt <liujiangtao.ljt@alibaba-inc.com>
2025-07-14 19:44:45 +0800

    add dashscope sdk stream

--- src/dashscope_demo/dashscope/dashscope_sdk_demo.py
+++ src/dashscope_demo/dashscope/dashscope_sdk_demo.py
@@ -1,4 +1,5 @@
 import os
+from ast import increment_lineno
 
 from dashscope import Generation
 
@@ -21,5 +22,22 @@ def main():
         print(f"错误信息：{response.message}")
         print("请参考文档：https://help.aliyun.com/zh/model-studio/developer-reference/error-code")
 
+
+def main_stream():
+    messages = [
+        {'role': 'system',
+         'content': 'You are a helpful assistant.'},
+        {'role': 'user', 'content': '你是谁？你能做什么？'}
+    ]
+
+    responses = Generation.call(
+        api_key=os.environ['AI_DASHSCOPE_API_KEY'], model=Generation.Models.qwen_plus, messages=messages,
+                           result_format="message", stream=True, incremental_output=True)
+
+    for response in responses:
+        print(response)
+        print(response.output.choices[0].message.content)
+
 if __name__ == '__main__':
     main()
+    main_stream()
